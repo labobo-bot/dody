2c4096aa49fd55494ac7f104b23a1b20ff9300d1
شاتنا <lm656508@gmail.com>
2026-05-22 01:34:14 +0300

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <div>
+      <h1>تطبيق دودي شغال ✅</h1>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
