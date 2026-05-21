4d411b32c78194b344e2bcacfb68d3f9f3d7e8b4
شاتنا <lm656508@gmail.com>
2026-05-22 01:34:51 +0300

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,10 +1,16 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+function App() {
+  return (
+    <div>
+      <h1>تطبيق دودي شغال ✅</h1>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-);
+  </React.StrictMode>
+)
